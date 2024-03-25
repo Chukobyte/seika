0ab89bcedb1970e569f609a6d622e7bf23518392
Chukobyte <michaelsmith8293@gmail.com>
2024-03-25 00:47:57 -0400

    Update.

---
+++ examples/demo_test/main.c
@@ -0,0 +1,16 @@
+#include <seika/seika.h>
+
+int main() {
+    if (!ska_init_all("test", 800, 600, 800, 600)) {
+        return -1;
+    }
+
+    while (ska_is_running()) {
+        ska_update();
+        ska_window_render();
+    }
+
+    ska_shutdown_all();
+
+    return 0;
+}
