1a1d42810565222bb90482704175f3f6dcc1e584
Chukobyte <chukobyte@gmail.com>
2024-10-17 23:08:19 -0400

    Added input.

---
+++ seika/input/input.h
@@ -0,0 +1,296 @@
+#pragma once
+
+#if SKA_INPUT
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "seika/math/math.h"
+
+typedef int32 SkaInputDeviceIndex;
+typedef uint32 SkaInputSubscriberHandle;
+typedef uint32 SkaInputActionHandle;
+typedef float SkaInputKeyStrength;
+
+#define SKA_INPUT_MAX_DEVICES ((SkaInputDeviceIndex)8)
+#define SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX ((SkaInputDeviceIndex)0)
+#define SKA_INPUT_MAX_INPUT_ACTIONS ((SkaInputActionHandle)128)
+#define SKA_INPUT_INVALID_INPUT_ACTION_HANDLE ((SkaInputActionHandle)-1)
+#define SKA_INPUT_MAX_INPUT_ACTION_VALUES 8
+
+typedef enum SkaInputSourceType {
+    SkaInputSourceType_INVALID,
+    SkaInputSourceType_KEYBOARD,
+    SkaInputSourceType_GAMEPAD,
+    SkaInputSourceType_MOUSE,
+} SkaInputSourceType;
+
+typedef enum SkaInputTriggerType {
+    SkaInputTriggerType_INVALID,
+    SkaInputTriggerType_PRESSED,
+    SkaInputTriggerType_RELEASED,
+    SkaInputTriggerType_AXIS_IN_MOTION,
+    SkaInputTriggerType_AXIS_STOPPED_MOTION,
+    SkaInputTriggerType_DEVICE_ADDED,
+    SkaInputTriggerType_DEVICE_REMOVED,
+} SkaInputTriggerType;
+
+typedef enum SkaInputInteractionStatus {
+    SkaInputInteractionStatus_PRESSED,
+    SkaInputInteractionStatus_JUST_PRESSED,
+    SkaInputInteractionStatus_JUST_RELEASED,
+    SkaInputInteractionStatus_AXIS_STARTED_MOTION,
+    SkaInputInteractionStatus_AXIS_IN_MOTION,
+    SkaInputInteractionStatus_AXIS_STOPPED_MOTION,
+} SkaInputInteractionStatus;
+
+typedef enum SkaInputKey {
+    SkaInputKey_INVALID,
+    // Gamepad
+    SkaInputKey_GAMEPAD_DPAD_DOWN,
+    SkaInputKey_GAMEPAD_DPAD_UP,
+    SkaInputKey_GAMEPAD_DPAD_LEFT,
+    SkaInputKey_GAMEPAD_DPAD_RIGHT,
+    SkaInputKey_GAMEPAD_FACE_BUTTON_NORTH,  // XBOX Y
+    SkaInputKey_GAMEPAD_FACE_BUTTON_SOUTH,  // XBOX A
+    SkaInputKey_GAMEPAD_FACE_BUTTON_EAST,   // XBOX B
+    SkaInputKey_GAMEPAD_FACE_BUTTON_WEST,   // XBOX X
+    SkaInputKey_GAMEPAD_START,
+    SkaInputKey_GAMEPAD_BACK,
+    SkaInputKey_GAMEPAD_LEFT_SHOULDER,      // PS L1
+    SkaInputKey_GAMEPAD_LEFT_TRIGGER,       // PS L2
+    SkaInputKey_GAMEPAD_LEFT_ANALOG_BUTTON, // PS L3
+    SkaInputKey_GAMEPAD_LEFT_ANALOG_2D_AXIS_X,
+    SkaInputKey_GAMEPAD_LEFT_ANALOG_2D_AXIS_Y,
+    SkaInputKey_GAMEPAD_RIGHT_SHOULDER,      // PS R1
+    SkaInputKey_GAMEPAD_RIGHT_TRIGGER,       // PS R2
+    SkaInputKey_GAMEPAD_RIGHT_ANALOG_BUTTON, // PS R3
+    SkaInputKey_GAMEPAD_RIGHT_ANALOG_2D_AXIS_X,
+    SkaInputKey_GAMEPAD_RIGHT_ANALOG_2D_AXIS_Y,
+    // Keeping these separate as 'RegisterInputEvent' should only use 'SkaInputKey_GAMEPAD_LEFT_ANALOG_2D_AXIS_X', 'SkaInputKey_GAMEPAD_LEFT_ANALOG_2D_AXIS_Y',
+    // 'SkaInputKey_GAMEPAD_RIGHT_ANALOG_2D_AXIS_X', and 'SkaInputKey_GAMEPAD_RIGHT_ANALOG_2D_AXIS_Y'.
+    // The key states for these keys will be updated automatically by the input system.
+    SkaInputKey_GAMEPAD_LEFT_ANALOG_LEFT,
+    SkaInputKey_GAMEPAD_LEFT_ANALOG_RIGHT,
+    SkaInputKey_GAMEPAD_LEFT_ANALOG_UP,
+    SkaInputKey_GAMEPAD_LEFT_ANALOG_DOWN,
+    SkaInputKey_GAMEPAD_RIGHT_ANALOG_LEFT,
+    SkaInputKey_GAMEPAD_RIGHT_ANALOG_RIGHT,
+    SkaInputKey_GAMEPAD_RIGHT_ANALOG_UP,
+    SkaInputKey_GAMEPAD_RIGHT_ANALOG_DOWN,
+
+    // Keyboard
+    SkaInputKey_KEYBOARD_TAB,
+    SkaInputKey_KEYBOARD_LEFT,
+    SkaInputKey_KEYBOARD_RIGHT,
+    SkaInputKey_KEYBOARD_UP,
+    SkaInputKey_KEYBOARD_DOWN,
+    SkaInputKey_KEYBOARD_PAGE_DOWN,
+    SkaInputKey_KEYBOARD_PAGE_UP,
+    SkaInputKey_KEYBOARD_HOME,
+    SkaInputKey_KEYBOARD_END,
+    SkaInputKey_KEYBOARD_INSERT,
+    SkaInputKey_KEYBOARD_DELETE,
+    SkaInputKey_KEYBOARD_BACKSPACE,
+    SkaInputKey_KEYBOARD_SPACE,
+    SkaInputKey_KEYBOARD_RETURN,
+    SkaInputKey_KEYBOARD_ESCAPE,
+    SkaInputKey_KEYBOARD_QUOTE,
+    SkaInputKey_KEYBOARD_COMMA,
+    SkaInputKey_KEYBOARD_MINUS,
+    SkaInputKey_KEYBOARD_PERIOD,
+    SkaInputKey_KEYBOARD_SLASH,
+    SkaInputKey_KEYBOARD_SEMICOLON,
+    SkaInputKey_KEYBOARD_EQUALS,
+    SkaInputKey_KEYBOARD_LEFT_BRACKET,
+    SkaInputKey_KEYBOARD_RIGHT_BRACKET,
+    SkaInputKey_KEYBOARD_BACKSLASH,
+    SkaInputKey_KEYBOARD_BACKQUOTE,
+    SkaInputKey_KEYBOARD_CAPS_LOCK,
+    SkaInputKey_KEYBOARD_SCROLL_LOCK,
+    SkaInputKey_KEYBOARD_NUM_LOCK_CLEAR,
+    SkaInputKey_KEYBOARD_PRINT_SCREEN,
+    SkaInputKey_KEYBOARD_PAUSE,
+    SkaInputKey_KEYBOARD_KEYPAD_0,
+    SkaInputKey_KEYBOARD_KEYPAD_1,
+    SkaInputKey_KEYBOARD_KEYPAD_2,
+    SkaInputKey_KEYBOARD_KEYPAD_3,
+    SkaInputKey_KEYBOARD_KEYPAD_4,
+    SkaInputKey_KEYBOARD_KEYPAD_5,
+    SkaInputKey_KEYBOARD_KEYPAD_6,
+    SkaInputKey_KEYBOARD_KEYPAD_7,
+    SkaInputKey_KEYBOARD_KEYPAD_8,
+    SkaInputKey_KEYBOARD_KEYPAD_9,
+    SkaInputKey_KEYBOARD_KEYPAD_PERIOD,
+    SkaInputKey_KEYBOARD_KEYPAD_DIVIDE,
+    SkaInputKey_KEYBOARD_KEYPAD_MULTIPLY,
+    SkaInputKey_KEYBOARD_KEYPAD_MINUS,
+    SkaInputKey_KEYBOARD_KEYPAD_PLUS,
+    SkaInputKey_KEYBOARD_KEYPAD_ENTER,
+    SkaInputKey_KEYBOARD_KEYPAD_EQUALS,
+    SkaInputKey_KEYBOARD_LEFT_CONTROL,
+    SkaInputKey_KEYBOARD_LEFT_SHIFT,
+    SkaInputKey_KEYBOARD_LEFT_ALT,
+    SkaInputKey_KEYBOARD_LEFT_GUI,
+    SkaInputKey_KEYBOARD_RIGHT_CONTROL,
+    SkaInputKey_KEYBOARD_RIGHT_SHIFT,
+    SkaInputKey_KEYBOARD_RIGHT_ALT,
+    SkaInputKey_KEYBOARD_RIGHT_GUI,
+    SkaInputKey_KEYBOARD_APPLICATION,
+    SkaInputKey_KEYBOARD_NUM_0,
+    SkaInputKey_KEYBOARD_NUM_1,
+    SkaInputKey_KEYBOARD_NUM_2,
+    SkaInputKey_KEYBOARD_NUM_3,
+    SkaInputKey_KEYBOARD_NUM_4,
+    SkaInputKey_KEYBOARD_NUM_5,
+    SkaInputKey_KEYBOARD_NUM_6,
+    SkaInputKey_KEYBOARD_NUM_7,
+    SkaInputKey_KEYBOARD_NUM_8,
+    SkaInputKey_KEYBOARD_NUM_9,
+    SkaInputKey_KEYBOARD_A,
+    SkaInputKey_KEYBOARD_B,
+    SkaInputKey_KEYBOARD_C,
+    SkaInputKey_KEYBOARD_D,
+    SkaInputKey_KEYBOARD_E,
+    SkaInputKey_KEYBOARD_F,
+    SkaInputKey_KEYBOARD_G,
+    SkaInputKey_KEYBOARD_H,
+    SkaInputKey_KEYBOARD_I,
+    SkaInputKey_KEYBOARD_J,
+    SkaInputKey_KEYBOARD_K,
+    SkaInputKey_KEYBOARD_L,
+    SkaInputKey_KEYBOARD_M,
+    SkaInputKey_KEYBOARD_N,
+    SkaInputKey_KEYBOARD_O,
+    SkaInputKey_KEYBOARD_P,
+    SkaInputKey_KEYBOARD_Q,
+    SkaInputKey_KEYBOARD_R,
+    SkaInputKey_KEYBOARD_S,
+    SkaInputKey_KEYBOARD_T,
+    SkaInputKey_KEYBOARD_U,
+    SkaInputKey_KEYBOARD_V,
+    SkaInputKey_KEYBOARD_W,
+    SkaInputKey_KEYBOARD_X,
+    SkaInputKey_KEYBOARD_Y,
+    SkaInputKey_KEYBOARD_Z,
+    SkaInputKey_KEYBOARD_F1,
+    SkaInputKey_KEYBOARD_F2,
+    SkaInputKey_KEYBOARD_F3,
+    SkaInputKey_KEYBOARD_F4,
+    SkaInputKey_KEYBOARD_F5,
+    SkaInputKey_KEYBOARD_F6,
+    SkaInputKey_KEYBOARD_F7,
+    SkaInputKey_KEYBOARD_F8,
+    SkaInputKey_KEYBOARD_F9,
+    SkaInputKey_KEYBOARD_F10,
+    SkaInputKey_KEYBOARD_F11,
+    SkaInputKey_KEYBOARD_F12,
+    SkaInputKey_KEYBOARD_F13,
+    SkaInputKey_KEYBOARD_F14,
+    SkaInputKey_KEYBOARD_F15,
+    SkaInputKey_KEYBOARD_F16,
+    SkaInputKey_KEYBOARD_F17,
+    SkaInputKey_KEYBOARD_F18,
+    SkaInputKey_KEYBOARD_F19,
+    SkaInputKey_KEYBOARD_F20,
+    SkaInputKey_KEYBOARD_F21,
+    SkaInputKey_KEYBOARD_F22,
+    SkaInputKey_KEYBOARD_F23,
+    SkaInputKey_KEYBOARD_F24,
+    SkaInputKey_KEYBOARD_APP_FORWARD,
+    SkaInputKey_KEYBOARD_APP_BACK,
+
+    // Mouse
+    SkaInputKey_MOUSE_BUTTON_LEFT,
+    SkaInputKey_MOUSE_BUTTON_RIGHT,
+    SkaInputKey_MOUSE_BUTTON_MIDDLE,
+
+    SkaInputKey_NUMBER_OF_KEYS
+} SkaInputKey;
+
+#define SKA_INPUT_KEY_GAMEPAD_ENUM_START SkaInputKey_GAMEPAD_DPAD_DOWN
+#define SKA_INPUT_KEY_GAMEPAD_ENUM_END SkaInputKey_GAMEPAD_RIGHT_ANALOG_DOWN
+#define SKA_INPUT_KEY_KEYBOARD_ENUM_START SkaInputKey_KEYBOARD_TAB
+#define SKA_INPUT_KEY_KEYBOARD_ENUM_END SkaInputKey_KEYBOARD_APP_BACK
+#define SKA_INPUT_KEY_MOUSE_ENUM_START SkaInputKey_MOUSE_BUTTON_LEFT
+#define SKA_INPUT_KEY_MOUSE_ENUM_END SkaInputKey_MOUSE_BUTTON_MIDDLE
+
+#define SKA_INPUT_KEY_GAMEPAD_COUNT (SKA_INPUT_KEY_GAMEPAD_ENUM_END - SKA_INPUT_KEY_GAMEPAD_ENUM_START)
+#define SKA_INPUT_KEY_KEYBOARD_COUNT (SKA_INPUT_KEY_KEYBOARD_ENUM_END - SKA_INPUT_KEY_KEYBOARD_ENUM_START)
+#define SKA_INPUT_KEY_MOUSE_COUNT (SKA_INPUT_KEY_MOUSE_ENUM_END - SKA_INPUT_KEY_MOUSE_ENUM_START)
+
+#define SKA_INPUT_KEY_GAMEPAD_FOR_EACH(KeyName) for(SkaInputKey KeyName = SKA_INPUT_KEY_GAMEPAD_ENUM_START; KeyName <= SKA_INPUT_KEY_GAMEPAD_ENUM_END; KeyName++)
+#define SKA_INPUT_KEY_KEYBOARD_FOR_EACH(KeyName) for(SkaInputKey KeyName = SKA_INPUT_KEY_KEYBOARD_ENUM_START; KeyName <= SKA_INPUT_KEY_KEYBOARD_ENUM_END; KeyName++)
+#define SKA_INPUT_KEY_MOUSE_FOR_EACH(KeyName) for(SkaInputKey KeyName = SKA_INPUT_KEY_MOUSE_ENUM_START; KeyName <= SKA_INPUT_KEY_MOUSE_ENUM_END; KeyName++)
+
+typedef enum SkaInputAxis {
+    SkaInputAxis_LEFT,
+    SkaInputAxis_RIGHT,
+} SkaInputAxis;
+
+typedef struct SkaMouse {
+    SkaVector2 position;
+} SkaMouse;
+
+typedef struct SkaInputEvent {
+    SkaInputSourceType sourceType;
+    SkaInputInteractionStatus interactionStatus;
+    SkaInputKey key;
+    SkaInputDeviceIndex deviceIndex;
+    f32 gamepadAxisValue;
+} SkaInputEvent;
+
+typedef struct SkaInputActionValue {
+    SkaInputKey key;
+    SkaInputKeyStrength strengthThreshold;
+} SkaInputActionValue;
+
+typedef struct SkaInputAction {
+    char* name;
+    SkaInputActionValue actionValues[SKA_INPUT_MAX_INPUT_ACTION_VALUES];
+    usize actionValuesCount;
+} SkaInputAction;
+
+const char* ska_input_key_to_string(SkaInputKey key);
+SkaInputKey ska_input_string_to_key(const char* keyName);
+
+bool ska_input_is_keyboard_key(SkaInputKey key);
+bool ska_input_is_mouse_key(SkaInputKey key);
+bool ska_input_is_gamepad_key(SkaInputKey key);
+
+// Events
+void ska_input_register_gamepad_added_event(SkaInputDeviceIndex deviceIndex);
+void ska_input_register_gamepad_removed_event(SkaInputDeviceIndex deviceIndex);
+void ska_input_register_input_event(SkaInputSourceType sourceType, SkaInputKey key, SkaInputTriggerType triggerType, SkaInputDeviceIndex deviceIndex, f32 gamepadAxisValue);
+void ska_input_register_input_event2(SkaInputSourceType sourceType, SkaInputKey key, SkaInputTriggerType triggerType, SkaInputDeviceIndex deviceIndex);
+void ska_input_register_input_event3(SkaInputSourceType sourceType, SkaInputKey key, SkaInputTriggerType triggerType);
+
+bool ska_input_is_key_pressed(SkaInputKey key, SkaInputDeviceIndex deviceIndex);
+bool ska_input_is_key_just_pressed(SkaInputKey key, SkaInputDeviceIndex deviceIndex);
+bool ska_input_is_key_just_released(SkaInputKey key, SkaInputDeviceIndex deviceIndex);
+f32 ska_input_get_key_strength(SkaInputKey key, SkaInputDeviceIndex deviceIndex);
+SkaVector2 ska_input_get_axis_input(SkaInputAxis axis, SkaInputDeviceIndex deviceIndex);
+
+SkaMouse* ska_input_get_mouse();
+
+// Input Action
+SkaInputActionHandle ska_input_add_input_action(const char* actionName, const SkaInputActionValue* actionValues, SkaInputDeviceIndex deviceIndex);
+SkaInputAction* ska_input_get_input_action(SkaInputActionHandle handle, SkaInputDeviceIndex deviceIndex);
+bool ska_input_remove_input_action(SkaInputActionHandle handle, SkaInputDeviceIndex deviceIndex);
+SkaInputActionHandle ska_input_find_input_action_handle(const char* actionName, SkaInputDeviceIndex deviceIndex);
+
+bool ska_input_is_input_action_pressed(SkaInputActionHandle handle, SkaInputDeviceIndex deviceIndex);
+bool ska_input_is_input_action_just_pressed(SkaInputActionHandle handle, SkaInputDeviceIndex deviceIndex);
+bool ska_input_is_input_action_just_released(SkaInputActionHandle handle, SkaInputDeviceIndex deviceIndex);
+f32 ska_input_get_input_action_strength(SkaInputActionHandle handle, SkaInputDeviceIndex deviceIndex);
+
+void ska_input_new_frame();
+void ska_input_reset_gamepad(SkaInputDeviceIndex deviceIndex);
+void ska_input_reset(SkaInputDeviceIndex deviceIndex);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // if SKA_INPUT
--- test/test1.c
+++ test/test1.c
@@ -11,6 +11,10 @@
 #include "seika/ecs/ec_system.h"
 #endif
 
+#if SKA_INPUT
+#include "seika/input/input.h"
+#endif
+
 #define RESOURCES_PATH "test/resources"
 #define RESOURCES_PACK_PATH "test/resources/test.pck"
 
@@ -25,6 +29,10 @@ void seika_id_queue_test(void);
 void seika_ecs_test(void);
 #endif
 
+#if SKA_INPUT
+void seika_input_test(void);
+#endif
+
 int32 main(int32 argv, char** args) {
     UNITY_BEGIN();
     RUN_TEST(seika_mem_test);
@@ -32,6 +40,9 @@ int32 main(int32 argv, char** args) {
     RUN_TEST(seika_id_queue_test);
 #if SKA_ECS
     RUN_TEST(seika_ecs_test);
+#endif
+#if SKA_INPUT
+    RUN_TEST(seika_input_test);
 #endif
     return UNITY_END();
 }
@@ -198,3 +209,92 @@ void seika_ecs_test(void) {
     ska_ecs_finalize();
 }
 #endif
+
+#if SKA_INPUT
+
+void seika_input_test(void) {
+    TEST_MESSAGE("Testing 'for each' and checking input type");
+    SKA_INPUT_KEY_GAMEPAD_FOR_EACH(key) {
+        TEST_ASSERT_TRUE(ska_input_is_gamepad_key(key));
+    }
+    SKA_INPUT_KEY_KEYBOARD_FOR_EACH(key) {
+        TEST_ASSERT_TRUE(ska_input_is_keyboard_key(key));
+    }
+    SKA_INPUT_KEY_MOUSE_FOR_EACH(key) {
+        TEST_ASSERT_TRUE(ska_input_is_mouse_key(key));
+    }
+
+    const char* backspaceKeyName = ska_input_key_to_string(SkaInputKey_KEYBOARD_BACKSPACE);
+    const SkaInputKey backSpaceKey = ska_input_string_to_key(backspaceKeyName);
+    TEST_ASSERT_EQUAL_STRING("Backspace", backspaceKeyName);
+    TEST_ASSERT_EQUAL_INT(SkaInputKey_KEYBOARD_BACKSPACE, backSpaceKey);
+
+    TEST_MESSAGE("Testing events");
+    ska_input_register_input_event3(SkaInputSourceType_KEYBOARD, SkaInputKey_KEYBOARD_BACKSPACE, SkaInputTriggerType_PRESSED);
+    TEST_ASSERT_TRUE(ska_input_is_key_pressed(SkaInputKey_KEYBOARD_BACKSPACE, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX));
+    TEST_ASSERT_TRUE(ska_input_is_key_just_pressed(SkaInputKey_KEYBOARD_BACKSPACE, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX));
+    TEST_ASSERT_FALSE(ska_input_is_key_just_released(SkaInputKey_KEYBOARD_BACKSPACE, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX));
+
+    ska_input_register_input_event3(SkaInputSourceType_KEYBOARD, SkaInputKey_KEYBOARD_BACKSPACE, SkaInputTriggerType_PRESSED);
+    TEST_ASSERT_TRUE(ska_input_is_key_pressed(SkaInputKey_KEYBOARD_BACKSPACE, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX));
+    TEST_ASSERT_FALSE(ska_input_is_key_just_pressed(SkaInputKey_KEYBOARD_BACKSPACE, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX));
+    TEST_ASSERT_FALSE(ska_input_is_key_just_released(SkaInputKey_KEYBOARD_BACKSPACE, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX));
+
+    ska_input_register_input_event3(SkaInputSourceType_KEYBOARD, SkaInputKey_KEYBOARD_BACKSPACE, SkaInputTriggerType_RELEASED);
+    TEST_ASSERT_FALSE(ska_input_is_key_pressed(SkaInputKey_KEYBOARD_BACKSPACE, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX));
+    TEST_ASSERT_FALSE(ska_input_is_key_just_pressed(SkaInputKey_KEYBOARD_BACKSPACE, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX));
+    TEST_ASSERT_TRUE(ska_input_is_key_just_released(SkaInputKey_KEYBOARD_BACKSPACE, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX));
+
+    TEST_MESSAGE("Testing input get axis input");
+    ska_input_register_input_event(SkaInputSourceType_GAMEPAD, SkaInputKey_GAMEPAD_LEFT_ANALOG_2D_AXIS_Y, SkaInputTriggerType_AXIS_IN_MOTION, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX, -0.5f);
+    const SkaVector2 firstAxisInput = ska_input_get_axis_input(SkaInputAxis_LEFT, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX);
+    TEST_ASSERT_EQUAL_FLOAT(0.0f, firstAxisInput.x);
+    TEST_ASSERT_EQUAL_FLOAT(-0.5f, firstAxisInput.y);
+
+    ska_input_register_input_event(SkaInputSourceType_GAMEPAD, SkaInputKey_GAMEPAD_LEFT_ANALOG_2D_AXIS_X, SkaInputTriggerType_AXIS_IN_MOTION, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX, 0.25f);
+    const SkaVector2 secondAxisInput = ska_input_get_axis_input(SkaInputAxis_LEFT, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX);
+    TEST_ASSERT_EQUAL_FLOAT(0.25f, secondAxisInput.x);
+    TEST_ASSERT_EQUAL_FLOAT(-0.5f, secondAxisInput.y);
+
+    ska_input_register_input_event(SkaInputSourceType_GAMEPAD, SkaInputKey_GAMEPAD_LEFT_ANALOG_2D_AXIS_X, SkaInputTriggerType_AXIS_STOPPED_MOTION, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX, 0.25f);
+    ska_input_register_input_event(SkaInputSourceType_GAMEPAD, SkaInputKey_GAMEPAD_LEFT_ANALOG_2D_AXIS_Y, SkaInputTriggerType_AXIS_STOPPED_MOTION, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX, 0.25f);
+    const SkaVector2 thirdAxisInput = ska_input_get_axis_input(SkaInputAxis_LEFT, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX);
+    TEST_ASSERT_EQUAL_FLOAT(0.0f, thirdAxisInput.x);
+    TEST_ASSERT_EQUAL_FLOAT(0.0f, thirdAxisInput.y);
+
+    ska_input_register_input_event(SkaInputSourceType_GAMEPAD, SkaInputKey_GAMEPAD_RIGHT_ANALOG_2D_AXIS_X, SkaInputTriggerType_AXIS_IN_MOTION, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX, 1.0f);
+    const SkaVector2 fourthAxisInput = ska_input_get_axis_input(SkaInputAxis_RIGHT, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX);
+    TEST_ASSERT_EQUAL_FLOAT(1.0f, fourthAxisInput.x);
+    TEST_ASSERT_EQUAL_FLOAT(0.0f, fourthAxisInput.y);
+
+    TEST_MESSAGE("Testing input actions");
+    SkaInputActionHandle jumpActionHandle = ska_input_add_input_action(
+        "jump",
+        (SkaInputActionValue[]){ { .key = SkaInputKey_KEYBOARD_SPACE, .strengthThreshold = 0.5f }, { .key = SkaInputKey_KEYBOARD_RETURN, .strengthThreshold = 0.5f }, { SkaInputKey_INVALID } },
+        SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX
+    );
+    TEST_ASSERT_NOT_EQUAL_UINT32(SKA_INPUT_INVALID_INPUT_ACTION_HANDLE, jumpActionHandle);
+    jumpActionHandle = ska_input_find_input_action_handle("jump", SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX);
+    TEST_ASSERT_NOT_EQUAL_UINT32(SKA_INPUT_INVALID_INPUT_ACTION_HANDLE, jumpActionHandle);
+
+    ska_input_register_input_event(SkaInputSourceType_KEYBOARD, SkaInputKey_KEYBOARD_RETURN, SkaInputTriggerType_PRESSED, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX, 0.0f);
+    TEST_ASSERT_TRUE(ska_input_is_input_action_pressed(jumpActionHandle, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX));
+    TEST_ASSERT_TRUE(ska_input_is_input_action_just_pressed(jumpActionHandle, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX));
+    TEST_ASSERT_FALSE(ska_input_is_input_action_just_released(jumpActionHandle, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX));
+    TEST_ASSERT_EQUAL_FLOAT(1.0f, ska_input_get_input_action_strength(jumpActionHandle, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX));
+
+    ska_input_new_frame();
+    TEST_ASSERT_TRUE(ska_input_is_input_action_pressed(jumpActionHandle, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX));
+    TEST_ASSERT_FALSE(ska_input_is_input_action_just_pressed(jumpActionHandle, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX));
+    TEST_ASSERT_FALSE(ska_input_is_input_action_just_released(jumpActionHandle, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX));
+
+    ska_input_register_input_event(SkaInputSourceType_KEYBOARD, SkaInputKey_KEYBOARD_RETURN, SkaInputTriggerType_RELEASED, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX, 0.0f);
+    TEST_ASSERT_FALSE(ska_input_is_input_action_pressed(jumpActionHandle, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX));
+    TEST_ASSERT_FALSE(ska_input_is_input_action_just_pressed(jumpActionHandle, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX));
+    TEST_ASSERT_TRUE(ska_input_is_input_action_just_released(jumpActionHandle, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX));
+
+    ska_input_remove_input_action(jumpActionHandle, SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX);
+    TEST_ASSERT_EQUAL_UINT32(SKA_INPUT_INVALID_INPUT_ACTION_HANDLE, ska_input_find_input_action_handle("jump", SKA_INPUT_FIRST_PLAYER_DEVICE_INDEX));
+}
+
+#endif // if SKA_INPUT
